793d3186ba8a5cc1350789a6eff64eaca6b9644f
juhani.kuru <juhanialeksikuru@gmail.com>
2026-03-09 13:31:18 +0200

    - use host: 0.0.0.0 for the api to be accessible on every network adapter

--- api.py
+++ api.py
@@ -10,4 +10,4 @@ load_dotenv()
 app.add_url_rule('/api/v1/accounts', 'get_accounts', controllers.accounts.get_all, methods=['GET'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
